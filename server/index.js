a9f3d3b0b17f8ab997013b331891e875d8ae2e85
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-06-03 14:16:32 +0800

    Setup backend server and mongodb

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Error: ", error);
+    process.exit(1);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
