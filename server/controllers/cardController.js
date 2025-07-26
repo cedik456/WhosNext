b963afe47a2c0bca7e8b33444a04756000b6bfbb
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-07-26 17:27:04 +0800

    Update cardController.js

--- server/controllers/cardController.js
+++ server/controllers/cardController.js
@@ -185,7 +185,7 @@ exports.getRecommendationsv2 = async (req, res) => {
       // preferences
 
       const preferences = jobSeeker.preferences || {};
-      const query = { userId: { $nin: swipeIds }, isOnboarded: true };
+      const query = { userId: { $nin: swipeIds } };
 
       const filterConditions = [];
 
@@ -255,7 +255,7 @@ exports.getRecommendationsv2 = async (req, res) => {
       // filters
 
       const filters = recruiter.filters || {};
-      const query = { userId: { $nin: swipeIds }, isOnboarded: true };
+      const query = { userId: { $nin: swipeIds } };
 
       const filterConditions = [];
 
