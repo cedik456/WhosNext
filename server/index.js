aa87ea220dcb81385cf08b5d39b5526d0e36503a
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-07-30 15:55:52 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // middlewares
 
