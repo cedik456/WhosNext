bce54d123a698a313d039f629214aac9820c42ee
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-08-06 08:15:29 +0800

    Add screens and finalized dark mode

--- client/app/(tabs)/profile/privacySafety.jsx
+++ client/app/(tabs)/profile/privacySafety.jsx
@@ -1,10 +1,32 @@
-import { View, Text } from "react-native";
+import { FontAwesome6 } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { useColorScheme } from "nativewind";
+import { View, Text, Pressable } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const PrivacyAndSafety = () => {
+  const { colorScheme } = useColorScheme();
+
   return (
-    <View>
-      <Text>PrivacyAndSafety</Text>
-    </View>
+    <SafeAreaView className="flex-1 dark:bg-black">
+      <View className="relative flex-row items-center justify-center px-6 mt-5">
+        <Pressable onPress={() => router.back()} className="absolute left-6 ">
+          <FontAwesome6
+            name="chevron-left"
+            size={24}
+            color={colorScheme === "dark" ? "white" : "black"}
+          />
+        </Pressable>
+
+        <Text
+          className={`${
+            colorScheme === "dark" ? "text-white" : "text-black"
+          } text-2xl  font-poppins-600 `}
+        >
+          Privacy & Safety
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
