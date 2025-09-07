cae6594d541c7fedaf4c400d7e4b3bc94d7aadbc
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-09-07 16:43:06 +0800

    Update socket js

--- client/utils/socket.js
+++ client/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://192.168.1.6:3000", {
+const socket = io("https://whosnextbackend.onrender.com", {
   transports: ["websocket"],
 });
 
